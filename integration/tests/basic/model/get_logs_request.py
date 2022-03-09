21d4d81e3e1055ce39a6efc295966a4d794005ab
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2022-03-09 15:05:49 +0300

    renaming of fields in models

--- integration/tests/basic/model/get_logs_request.py
+++ integration/tests/basic/model/get_logs_request.py
@@ -3,13 +3,11 @@ from dataclasses_json import LetterCase, dataclass_json
 from typing import List, Union
 
 
-@dataclass_json #(letter_case=LetterCase.CAMEL)
+@dataclass_json(letter_case=LetterCase.CAMEL)
 @dataclass
 class GetLogsRequest:
-    # fromBlock: Union[int, str] = None
-    # toBlock: Union[int, str] = None
     from_block: Union[int, str] = None
     to_block: Union[int, str] = None
-    address: List[str] = None
+    address: Union[str, List[str]] = None
     topics: List[str] = None
     blockhash: List[str] = None
