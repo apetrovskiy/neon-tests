6825edc118a0d591e413806ed38a070cf1e31c28
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2022-03-09 14:15:32 +0300

    experimental changes

--- integration/tests/basic/model/get_logs_request.py
+++ integration/tests/basic/model/get_logs_request.py
@@ -1,11 +1,15 @@
 from dataclasses import dataclass
+from dataclasses_json import LetterCase, dataclass_json
 from typing import List, Union
 
 
+@dataclass_json #(letter_case=LetterCase.CAMEL)
 @dataclass
 class GetLogsRequest:
-    fromBlock: Union[int, str] = None
-    toBlock: Union[int, str] = None
+    # fromBlock: Union[int, str] = None
+    # toBlock: Union[int, str] = None
+    from_block: Union[int, str] = None
+    to_block: Union[int, str] = None
     address: List[str] = None
     topics: List[str] = None
     blockhash: List[str] = None
