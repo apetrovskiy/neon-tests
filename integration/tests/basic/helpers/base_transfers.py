fe9abf136035684bce008e83f39327c2edf969b2
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2022-03-10 15:34:06 +0300

    refactoring

---
+++ integration/tests/basic/helpers/base_transfers.py
@@ -0,0 +1,16 @@
+import pytest
+from eth_account import Account
+from integration.tests.basic.helpers.basic_helpers import BasicHelpers
+
+
+class BaseTransfers(BasicHelpers):
+    # sender_account: Account
+    # recipient_account: Account
+
+    @pytest.fixture
+    # (autouse=True,scope="class")
+    # @pytest.fixture(scope="class")
+    def prepare_accounts(self):
+        self.sender_account = self.create_account_with_balance()
+        self.recipient_account = self.create_account_with_balance()
+        yield
