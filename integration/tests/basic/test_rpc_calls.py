86e39068c86ff24e13e11a8af668d1ecee398a5e
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2022-03-11 09:46:40 +0300

    Update test_rpc_calls.py

--- integration/tests/basic/test_rpc_calls.py
+++ integration/tests/basic/test_rpc_calls.py
@@ -94,17 +94,38 @@ class TestRpcCalls(BasicTests):
                           JsonRpcResponse), AssertMessage.WRONG_TYPE.value
         assert '0x' in actual_result.result, AssertMessage.DOES_NOT_START_WITH_0X.value
 
-    # TOOD: implement variants
     @allure.step("test: verify implemented rpc calls work eth_getLogs")
     @pytest.mark.parametrize("from_block,to_block", GET_LOGS_TEST_DATA)
-    def test_rpc_call_eth_getLogs(self, from_block: Tag, to_block: Tag):
+    def test_rpc_call_eth_getLogs_via_tags(self, from_block: Tag,
+                                           to_block: Tag):
+        """Verify implemented rpc calls work eth_getLogs"""
+        # TODO: use contract instead of account
+        sender_account = self.create_account_with_balance()
+        params = [
+            GetLogsRequest(from_block=from_block,
+                           to_block=to_block,
+                           address=sender_account.address)
+        ]
+        model = RpcRequestFactory.get_logs(params=params)
+
+        response = self.jsonrpc_requester.request_json_rpc(model)
+        actual_result = self.jsonrpc_requester.deserialize_response(response)
+
+        assert actual_result.id == model.id, AssertMessage.WRONG_ID.value
+        assert self.assert_no_error_object(
+            actual_result), AssertMessage.CONTAINS_ERROR
+        assert self.assert_result_object(
+            actual_result), AssertMessage.DOES_NOT_CONTAIN_RESULT
+
+    @allure.step("test: verify implemented rpc calls work eth_getLogs")
+    def test_rpc_call_eth_getLogs_via_number(self):
         """Verify implemented rpc calls work eth_getLogs"""
         # TODO: use contract instead of account
         sender_account = self.create_account_with_balance()
         # TOOD: variants
         params = [
-            GetLogsRequest(from_block=from_block,
-                           to_block=to_block,
+            GetLogsRequest(from_block=1,
+                           to_block=Tag.LATEST.value,
                            address=sender_account.address)
         ]
         model = RpcRequestFactory.get_logs(params=params)
