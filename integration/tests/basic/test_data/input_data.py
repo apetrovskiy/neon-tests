427520e6804e31b86f4a4827c2a1d3f0344ed8d6
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2022-03-11 10:28:10 +0300

    Create input_data.py

---
+++ integration/tests/basic/test_data/input_data.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class InputData(Enum):
+    FIRST_FAUCET_REQUEST_AMOUNT = 5
+    SECOND_FAUCET_REQUEST_AMOUNT = 3
+    FIRST_AMOUNT_IN_RESPONSE = '0x4563918244f40000'
+    DEFAULT_TRANSFER_AMOUNT = 3
+    NEGATIVE_AMOUNT = -1
+    SAMPLE_AMOUNT = 4
+    ROUND_DIGITS = 3
+
+    def get_transfer_amount(self) -> int:
+        return self.DEFAULT_TRANSFER_AMOUNT.value
+
+    def get_default_initial_amount(self) -> int:
+        return self.FIRST_FAUCET_REQUEST_AMOUNT.value
