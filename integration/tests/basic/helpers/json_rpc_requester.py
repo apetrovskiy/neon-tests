6d8c091fec1c29465f895f03b9f9e583773980c2
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2022-03-14 10:35:47 +0300

    experimental changes

--- integration/tests/basic/helpers/json_rpc_requester.py
+++ integration/tests/basic/helpers/json_rpc_requester.py
@@ -14,25 +14,24 @@ class JsonRpcRequester:
 
     @allure.step("requesting Json-RPC")
     def request_json_rpc(self, data: JsonRpcRequest) -> Response:
-        with allure.step("getting response"):
-            return self._session.post(self._url, json=dataclasses.asdict(data))
+        return self._session.post(self._url, json=dataclasses.asdict(data))
 
     # TODO: deserialize subobject
-    @allure.step("deserializing response from JSON")
+    # @allure.step("deserializing response from JSON as model ot type {2}")
     def deserialize_response(
             self,
             response: Response,
             type: Type = None) -> Union[JsonRpcResponse, JsonRpcErrorResponse]:
         str_data = self.stringify(response.json())
-        with allure.step("deserialized"):
-            if 'result' in str_data:
-                return self.deserialize_successful_response(response=response,
-                                                            type=type)
-            elif 'error' in str_data:
-                return JsonRpcErrorResponse(**response.json())
-            else:
-                return JsonRpcErrorResponse(**response.json())
+        if 'result' in str_data:
+            return self.deserialize_successful_response(response=response,
+                                                        type=type)
+        elif 'error' in str_data:
+            return JsonRpcErrorResponse(**response.json())
+        else:
+            return JsonRpcErrorResponse(**response.json())
 
+    @allure.step("deserializing response from JSON as model ot type {2}")
     def deserialize_successful_response(self, response: Response,
                                         type: Type) -> JsonRpcResponse:
         json_rpc_response = JsonRpcResponse(**response.json())
