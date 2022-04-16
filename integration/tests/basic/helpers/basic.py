6b4e31cd55a3e3e7b122a5e19b2a09a592471d3b
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2022-04-16 20:41:36 +0300

    Update basic.py

--- integration/tests/basic/helpers/basic.py
+++ integration/tests/basic/helpers/basic.py
@@ -105,8 +105,14 @@ class BasicTests(BaseTests):
 
             if error_info != None:
 
+                #
+                print("=======")
+                print(error_info)
+                #
+
                 if error_message:
-                    assert error_message in str(error_info)
+                    assert error_message in str(
+                        error_info), f"Expected {error_message} to be in {error_info}"
                 assert None != error_info, "Transaction failed"
 
             return tx
