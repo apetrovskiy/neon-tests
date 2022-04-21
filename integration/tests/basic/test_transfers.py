fcd2b12cc5de974d7da22560384411aa7fcb9ead
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2022-04-21 19:58:23 +0300

    Update test_transfers.py

--- integration/tests/basic/test_transfers.py
+++ integration/tests/basic/test_transfers.py
@@ -84,17 +84,17 @@ class TestTransfer(BasicTests):
     def test_send_more_than_exist_on_account_erc20(self, erc20wrapper, prepare_accounts):
         """Send more than exist on account: ERC20"""
         initial_amount = 0
-        amount = 1_000_000_000_000_000_000  # U64_MAX + 1_000
+        amount = 1_000_000_000_000_000  # 10_000_000_000 + 1_000  # 1_000_000_000_000_000_000  # U64_MAX + 1_000
 
         contract, spl_owner = erc20wrapper
 
         assert contract.functions.balanceOf(self.recipient_account.address).call() == initial_amount
 
-        with pytest.raises("exceptions.ContractLogicError") as error_info:
-            transfer_tx = self.web3_client.send_erc20(
-                spl_owner, self.recipient_account, amount, contract.address, abi=contract.abi
-            )
-        assert None == error_info, f"Transaction failed: {error_info}"
+        # with pytest.raises("exceptions.ContractLogicError") as error_info:
+        transfer_tx = self.web3_client.send_erc20(
+            spl_owner, self.recipient_account, amount, contract.address, abi=contract.abi
+        )
+        # assert None == error_info, f"Transaction failed: {error_info}"
 
         assert contract.functions.balanceOf(self.recipient_account.address).call() == initial_amount
 
