7ffd99de35c6702a226e1ade594e33f87992f849
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2022-04-22 12:21:17 +0300

    Update test_single_client_activity.py

--- integration/tests/basic/test_single_client_activity.py
+++ integration/tests/basic/test_single_client_activity.py
@@ -1,7 +1,7 @@
 import allure
 import pytest
 
-from integration.tests.basic.helpers.basic import WAITING_FOR_ERC20, WAITING_FOR_MS, BaseMixin
+from integration.tests.basic.helpers.basic import  BaseMixin
 from integration.tests.basic.test_data.input_data import InputData
 
 """
