040304b43abee445e328717f5be319ab18f85227
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2022-05-16 11:47:57 +0300

    support for devnet faucet

--- utils/faucet.py
+++ utils/faucet.py
@@ -1,7 +1,8 @@
+import requests
 import typing as tp
 import urllib.parse
-
-import requests
+from busypie import wait_at_most, MINUTE
+from http import HTTPStatus
 
 
 class Faucet:
@@ -12,5 +13,9 @@ class Faucet:
     def request_neon(self, address: str, amount: int = 100):
         assert address.startswith("0x")
         url = urllib.parse.urljoin(self._url, "request_neon")
-        resp = self._session.post(url, json={"amount": amount, "wallet": address})
-        assert resp.status_code == 200, resp.text
+        # resp = self._session.post(url, json={"amount": amount, "wallet": address})
+        wait_at_most(MINUTE).until(lambda: HTTPStatus.OK == self.send_post_request(url, address, amount).status_code)
+        # assert resp.status_code == 200, resp.text
+
+    def send_post_request(self, url: str, address: str, amount: int):
+        return self._session.post(url, json={"amount": amount, "wallet": address})
