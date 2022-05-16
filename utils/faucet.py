d0effa19ccdbd20e086466155aa3320fad5a575a
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2022-05-16 14:42:39 +0300

    next changes

--- utils/faucet.py
+++ utils/faucet.py
@@ -1,7 +1,7 @@
 import requests
 import typing as tp
 import urllib.parse
-from busypie import wait_at_most, TEN_SECONDS
+from busypie import wait_at_most, ONE_SECOND, TEN_SECONDS
 from busypie.durations import TWO_MINUTES
 
 from datetime import datetime
@@ -17,7 +17,9 @@ class Faucet:
     def request_neon(self, address: str, amount: int = 100):
         assert address.startswith("0x")
         url = urllib.parse.urljoin(self._url, "request_neon")
-        wait_at_most(TWO_MINUTES).poll_interval(TEN_SECONDS).until(lambda: self.send_post_request(url, address, amount))
+        wait_at_most(TWO_MINUTES).poll_interval(TEN_SECONDS + ONE_SECOND).until(
+            lambda: self.send_post_request(url, address, amount)
+        )
 
     def send_post_request(self, url: str, address: str, amount: int) -> bool:
         response = self._session.post(url, json={"amount": amount, "wallet": address})
